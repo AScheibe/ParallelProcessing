97cba50b06e4bcd61cfb4efbce8f1002e7391a50
Richik Sinha Choudhury <sinhachoudhu@wisc.edu>
2023-04-13 22:26:48 -0500

    WIP

--- psort.c
+++ psort.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 
 typedef struct _thread_data_t {
     int tid;
@@ -11,8 +13,8 @@ typedef struct _thread_data_t {
 } thread_data_t;
 
 int keycmp(char* line1, char* line2){
-    char* key1[4];
-    char* key2[4];
+    char key1[4];
+    char key2[4];
 
     strncpy(key1, line1, 4);
     strncpy(key2, line2, 4);
@@ -24,16 +26,16 @@ void merging(int low, int mid, int high, char** lines, char** lines_ph) {
    int l1, l2, i;
 
    for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
-      if(keycmp(lines, lines_ph) <= 0)
+      if(keycmp(lines[l1], lines[l2]) <= 0)
          lines_ph[i] = lines[l1++];
       else
          lines_ph[i] = lines[l2++];
    }
-   
-   while(l1 <= mid)    
+
+   while(l1 <= mid)
       lines_ph[i++] = lines[l1++];
 
-   while(l2 <= high)   
+   while(l2 <= high)
       lines_ph[i++] = lines[l2++];
 
    for(i = low; i <= high; i++)
@@ -43,15 +45,15 @@ void merging(int low, int mid, int high, char** lines, char** lines_ph) {
 // low, high inclusive
 void sort(int low, int high, char** lines, char** lines_ph) {
    int mid;
-   
+
    if(low < high) {
       mid = (low + high) / 2;
       sort(low, mid, lines, lines_ph);
       sort(mid+1, high, lines, lines_ph);
       merging(low, mid, high, lines, lines_ph);
-   } else { 
+   } else {
       return;
-   }   
+   }
 }
 
 void *sort_thread(void* thr_data) {
@@ -66,17 +68,23 @@ void *sort_thread(void* thr_data) {
     sort(data->low, data->high, data->lines, lines_ph);
 
     free(lines_ph);
-} 
 
-void psort(char** lines, int num_lines, int num_threads){
+    pthread_exit(NULL);
+}
+
+int psort(char** lines, int num_lines, int num_threads){
     pthread_t threads[num_threads];
     thread_data_t thr_data[num_threads];
 
+    if (lines == NULL) {
+        return 1;
+    }
+
     int rc;
 
-    int chunk_size = num_lines / num_threads;
+    int chunk_size = 0;
 
-    if(chunk_size == 0){
+    if(num_threads == 0 || (chunk_size = num_lines/num_threads) == 0){
         char** lines_ph = malloc(num_lines * sizeof(char*));
         for(int i = 0; i < num_lines; i++){
             lines_ph[i] = malloc(100 * sizeof(char));
@@ -94,31 +102,29 @@ void psort(char** lines, int num_lines, int num_threads){
             } else {
                 thr_data[i].high = (i + 1) * chunk_size - 1; // one below start of next chunk
             }
-            
-            if ((rc = pthread_create(&threads[i], NULL, sort, &thr_data[i]))) {
+
+            if ((rc = pthread_create(&threads[i], NULL, sort_thread, &thr_data[i]))) {
                 fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
                 return -1;
             }
         }
-        
+
         /* block until all threads complete */
         for (int i = 0; i < num_threads; ++i) {
             pthread_join(threads[i], NULL);
         }
-        
+
         char** lines_ph = malloc(num_lines * sizeof(char*));
         for(int i = 0; i < num_lines; i++){
             lines_ph[i] = malloc(100 * sizeof(char));
         }
-    
-
 
         int low = 0;
         int high = 0;
 
         for(int i = 0; i < num_threads; i += 2){
             int mid = ((i + 1) * chunk_size) - 1; // one below start of next chunk
-            
+
             if (i == num_threads) {
                 high = num_lines - 1; // high index inclusive
             } else {
@@ -133,18 +139,25 @@ void psort(char** lines, int num_lines, int num_threads){
 
         free(lines_ph);
     }
+    return 0;
 }
 
 int main(int argc, char const *argv[])
 {
+    printf("Welcome to psort!\n");
     if(argc != 4){
-        printf("Invalid Input");
-        return;
+        perror("Invalid Input");
+        exit(EXIT_FAILURE);
     }
 
     // read in args
     FILE *fp_in = fopen(argv[1], "r");
     FILE *fp_out = fopen(argv[2], "w+");
+
+    if (fp_in == NULL || fp_out == NULL) {
+        perror("file failed to open");
+        exit(EXIT_FAILURE);
+    }
 
     int num_threads = atoi(argv[3]);
 
@@ -169,24 +182,38 @@ int main(int argc, char const *argv[])
 
     // copy lines in file to array
     size_t len = 0;
-    char* line;
+    char* line = NULL;
     int i = 0;
-    
-    while(getline(&line, &len, fp_in) != -1){
+    ssize_t nread = 0;
+    printf("code was recompiled!");
+
+    while ((nread = getline(&line, &len, fp_in)) != -1) {
+        printf("getline: %s\n", line);
         strcpy(lines[i], line);
-    }    
-
-    // sort lines
-    psort(lines, num_lines, num_threads);
-
-    // print to output file
-    for(int i = 0; i < num_lines; i++){
-        fprintf(fp_out, "%s\n", lines[i]);
+        i++;
     }
 
-    fsync();
+    free(line);
+
+
+
+    int retval = 0;
+
+    // sort lines
+    if (psort(lines, num_lines, num_threads) == 0) {
+        printf("psort success!\n");
+        // print to output file
+        for (int i = 0; i < num_lines; i++){
+            fprintf(fp_out, "%s\n", lines[i]);
+        }
+
+        fsync(fileno(fp_out));
+        retval = 1;
+    }
+
+
     fclose(fp_in);
     fclose(fp_out);
     free(lines);
-    return 0;   
+    return retval;
 }
